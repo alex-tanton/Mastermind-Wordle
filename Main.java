e6a574f8867bc780a8b88b2b6924fce926964be3
alex-tanton <alex.c.tanton@gmail.com>
2022-10-28 19:46:25 -0400

    oops, final file?

--- Main.java
+++ Main.java
@@ -1,6 +1,42 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        // Make 2 players
         Player p1 = new Player();
         Player p2 = new Player();
+
+        // Variables for loop
+        Scanner input = new Scanner(System.in);
+        String guess;
+        Player winner;
+        int turns = 1;
+
+        // Keep players guessing until one wins
+        while (true) {
+            // Take player 1 guess
+            System.out.print(p1.get_name() + ", make a guess: ");
+            guess = input.nextLine();
+
+            // Check if guess is correct, display known letters if incorrect
+            if (p2.get_word().make_guess(guess)) { winner = p1; break; }
+            p2.get_word().display();
+            System.out.println();
+
+            // Take player 2 guess
+            System.out.print(p2.get_name() + ", make a guess: ");
+            guess = input.nextLine();
+
+            // Check if guess is correct, display known letters if incorrect
+            if (p1.get_word().make_guess(guess)) { winner = p2; break; }
+            p1.get_word().display();
+            System.out.println();
+
+            turns++;
+        }
+
+        input.close();
+
+        System.out.println("\n" + winner.get_name() + " won in " + turns + " guesses!");
     }
 }
--- Word.java
+++ Word.java
@@ -7,21 +7,33 @@ public class Word {
         this.status = new boolean[str.length()];
     }
 
-    public boolean take_guess(String str) {
+    public boolean make_guess(String str) {
         // Set status at respective index true if word and guess char matches
         for (int i = 0; i < this.word.length(); i++) {
             if (this.word.charAt(i) == str.charAt(i)) {
-                status[i] = true;
+                this.status[i] = true;
             }
         }
 
         // Return true if all status true, false otherwise
-        for(boolean b : status) if(!b) return false;
+        for(boolean b : this.status) if(!b) return false;
         return true;
     }
 
+    public void display() {
+        for (int i = 0; i < this.word.length(); i++) {
+            if (this.status[i]) {
+                System.out.print(this.word.charAt(i));
+            } else {
+                System.out.print("_");
+            }
+            System.out.print(" ");
+        }
+        System.out.println();
+    }
+
     // Getters
-    public String get_word() {
+    public String get_string() {
         return this.word;
     }
     public boolean[] get_status() {
