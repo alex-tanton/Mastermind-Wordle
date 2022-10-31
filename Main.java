603479d27836788a4dd7e495cc5a58c940fdd56f
alex-tanton <alex.c.tanton@gmail.com>
2022-10-31 11:52:19 -0400

    readability improvements

--- Main.java
+++ Main.java
@@ -4,7 +4,7 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) {
         // Shows instructions for some time then clears
-        System.out.print("\033[H\033[2J");
+        clear_output();
         System.out.println("WELCOME TO MASTERMIND WORDLE");
         try {
             TimeUnit.SECONDS.sleep(3);
@@ -12,14 +12,14 @@ public class Main {
             System.out.println("Critical error in delay.");
             return;
         }
-        System.out.print("\033[H\033[2J");
+        clear_output();
 
         // Make 2 players
         Player p1 = new Player();
-        System.out.print("\033[H\033[2J");
+        clear_output();
 
         Player p2 = new Player();
-        System.out.print("\033[H\033[2J");
+        clear_output();
 
         // Variables for loop
         String guess;
@@ -34,7 +34,7 @@ public class Main {
             p2.get_word().display();
             System.out.print(p1.get_name() + ", make a guess: ");
             guess = input.nextLine().toLowerCase();
-            System.out.print("\033[H\033[2J");
+            clear_output();
 
             // Check if guess is correct, display known letters if incorrect
             if (p2.get_word().make_guess(guess)) { winner = p1; break; }
@@ -43,7 +43,7 @@ public class Main {
             p1.get_word().display();
             System.out.print(p2.get_name() + ", make a guess: ");
             guess = input.nextLine().toLowerCase();
-            System.out.print("\033[H\033[2J");
+            clear_output();
 
             // Check if guess is correct, display known letters if incorrect
             if (p1.get_word().make_guess(guess)) { winner = p2; break; }
@@ -55,4 +55,6 @@ public class Main {
 
         System.out.println(winner.get_name() + " won in " + turns + " guesses!");
     }
+
+    public static void clear_output() { System.out.print("\033[H\033[2J"); }
 }
